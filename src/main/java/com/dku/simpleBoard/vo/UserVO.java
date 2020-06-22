8b6deb00eaadf18b68dd167a53227f2f72655d7b
gudwnsgur <gudwnsgur12@naver.com>
2020-06-22 14:47:47 +0900

    [Modify] Array Version & [ADD] dependency for MySQL

--- src/main/java/com/dku/simpleBoard/vo/UserVO.java
+++ src/main/java/com/dku/simpleBoard/vo/UserVO.java
@@ -4,6 +4,7 @@ public class User {
     private String userId;
     private String userPwd;
     private String email;
+    private String phoneNumber;
 
     public String getEmail() {
         return email;
